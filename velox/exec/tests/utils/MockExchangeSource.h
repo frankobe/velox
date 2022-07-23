1995b623f44396664a17fa29c053dc4132c6c429
Zhongjun Jin <zhongjun.jin@bytedance.com>
2022-07-22 20:31:30 -0700

    fix format and headers

--- velox/exec/tests/utils/MockExchangeSource.h
+++ velox/exec/tests/utils/MockExchangeSource.h
@@ -1,12 +1,22 @@
-//
-// Created by Zhongjun Jin on 7/22/22.
-//
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include <folly/Uri.h>
 
 #include "velox/exec/Exchange.h"
 
-#ifndef VELOX_MOCHEXCHANGESOURCE_H
-#define VELOX_MOCHEXCHANGESOURCE_H
 using namespace facebook::velox;
 
 class MockExchangeSource : public exec::ExchangeSource {
@@ -30,5 +40,3 @@ class MockExchangeSource : public exec::ExchangeSource {
   void close() override;
   static std::vector<std::string> closedTasks_;
 };
-
-#endif // VELOX_MOCHEXCHANGESOURCE_H
